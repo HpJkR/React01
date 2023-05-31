2fa631d3c1238cd78b360fdd9a54368535f6b65d
HpJkR <m.hertereau@gmail.com>
2023-05-31 10:58:45 +0200

    ajout des commentaires sur le code

--- react01/src/components/PokemonCard.jsx
+++ react01/src/components/PokemonCard.jsx
@@ -1,25 +1,32 @@
+// Déclaration d'un tableau contenant des objets Pokémon
 const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      name: "bulbasaur", // Nom du premier Pokémon
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png", // URL de l'image du premier Pokémon
     },
     {
-      name: "mew",
+      name: "mew", // Nom du deuxième Pokémon
     },
   ];
-
-function PokemonCard() {
-  const pokemon = pokemonList[1];
-
-
-  return  (
-    <figure>
-        {pokemon.imgSrc != undefined ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
-      <figcaption>{pokemon.name}</figcaption>
-    </figure>
-  );
-}
-export default PokemonCard;
+  
+  // Définition du composant PokémonCard
+  function PokemonCard() {
+    const pokemon = pokemonList[1]; // Sélection du deuxième Pokémon dans la liste
+  
+    // Retourne un élément figure contenant le nom et l'image du Pokémon
+    return (
+      <figure>
+        {pokemon.imgSrc != undefined ? ( // Vérifie si l'URL de l'image est définie
+          <img src={pokemon.imgSrc} alt={pokemon.name} /> // Affiche l'image du Pokémon avec son nom en tant qu'attribut alt
+        ) : (
+          <p>???</p> // Affiche "???" si l'URL de l'image n'est pas définie
+        )}
+        <figcaption>{pokemon.name}</figcaption> 
+      </figure> // Affiche le nom du Pokémon
+    );
+  }
+  
+  export default PokemonCard; // Exporte le composant PokémonCard pour pouvoir l'utiliser dans d'autres modules
+  
 
 
