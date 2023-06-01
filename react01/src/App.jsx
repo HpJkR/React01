66f03f66be0e7b60e688243770a2eb478c4a6b5f
HpJkR <m.hertereau@gmail.com>
2023-06-01 10:54:41 +0200

    added button next and previous

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -4,27 +4,59 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
+
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 
 function App() {
 
-// Déclaration d'un tableau contenant des objets Pokémon
-const pokemonList = [
-  {
-    name: "bulbasaur", // Nom du premier Pokémon
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png", // URL de l'image du premier Pokémon
-  },
-  {
-    name: "mew", // Nom du deuxième Pokémon
-  },
-];
+  const [pokemonIndex, setPokemonIndex] = useState(0)
+
+  const handleClickBack = () => {
+    if (pokemonIndex > 0){
+    setPokemonIndex(pokemonIndex - 1)}
+  }
+  const handleClickNext = () => {
+    if (pokemonIndex < pokemonList.length - 1){
+    setPokemonIndex(pokemonIndex + 1)} 
+  }
+
+
   return (
     <>
       <h1>Attrapez les tous !</h1>
       <div className="card">
-      <PokemonCard pokemon = {pokemonList[0]}/>
+      <PokemonCard pokemon = {pokemonList[pokemonIndex]}/>
       </div>
+      {pokemonIndex > 0 ? <button onClick={handleClickBack}>Previous</button> : <p></p> }
+      {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickNext}>Next</button> : <p></p>}
     </>
   )
-  }
-
+}
+  
 export default App;
